2b1df29fb5990d28175cd8951ba4b93aa440af60
jyoti <jyotidhage10@gmail.com>
2022-09-09 17:00:01 +0530

    aaradhya practice line comparison problems

---
+++ LineComparison/LineComparison2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public static  class LineComparison2
+    {
+        public static void CheckEquality()
+        {
+            double x1, x2, y1, y2;
+
+            Console.WriteLine("Enter co-ordinates of First Line ");
+            Console.WriteLine("Enter the value of x1:");      //enter the input x1
+            x1 = Convert.ToInt32(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
+            Console.WriteLine("Enter the value of x2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1:");      //Enter the value y1
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
+
+            Console.WriteLine("Enter co-ordinates of Second Line ");
+            Console.WriteLine("Enter the value of x1:");      //enter the input x1
+            x1 = Convert.ToInt32(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
+            Console.WriteLine("Enter the value of x2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1:");      //Enter the value y1
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
+            int compare = Length1.CompareTo(Length2); //use compareto method
+            Boolean equal = Length1.Equals(Length2);
+
+
+            if (Length1.CompareTo(Length2) == 0)
+            {
+                 Console.WriteLine("By Comparing the Both Lines are Same");
+
+                if (Length1.Equals(Length2) == true)
+                {
+                    Console.WriteLine("By Comparing the Both Lines are Same");
+
+                    Console.WriteLine("True");
+                }
+                //else
+                // {
+
+                //  Console.WriteLine("False");
+                // }
+
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+
+            Console.WriteLine("The Length of Line1 is  " + Length1);  //finaly print output
+            Console.WriteLine("The Length of Line2 is  " + Length2);  //finaly print output
+            Console.Read();
+
+        }
+    }
+}
+ 
+        
+    
+
---
+++ LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+          //  Linecomparison1.LengthOfOneLine();
+            LineComparison2.CheckEquality();
+        }
+    }
+}
